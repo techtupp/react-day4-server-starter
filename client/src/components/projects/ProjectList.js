aecfc69d27ef35fa25cb4213c38ca93d7622e770
Tech Tupp <techtupp@gmail.com>
2022-02-02 21:32:24 +0100

    change

--- client/src/components/projects/ProjectList.js
+++ client/src/components/projects/ProjectList.js
@@ -30,51 +30,51 @@ class ProjectList extends Component {
     return (
       <table class="table">
         <div>
-          <div class='container'>
-            <tr>
-              <div>
-                <h2>
-                  Todo List
-                </h2>
-              </div>
-            </tr>
-            <tr>
-              <div style={{ width: '40%', float: "right" }}>
-                <AddProject getData={() => this.getAllProjects()} /> {/* <== !!! */}
-              </div>
-            </tr>
-            <tr>
-              <div class='columns is-mobile is-centered'>
-                <div class='column is-6'>
-                  <div class='list'>
-                    {this.state.listOfProjects.map(project => {
-                      return (
-                        <div key={project._id}>
-                          <Link to={`/projects/${project._id}`}>
-                            <td>
-                              <h3 class="list-item">{project.title}</h3>
-                            </td>
-                            <td>
-                              
-                            </td>
-                          </Link>
-                          {/* 🥁 added so the tasks can be displayed:  🥁 */}
-                          {/* <ul>
+
+          <tr>
+            <div>
+              <h1>
+                Todo List
+              </h1>
+            </div>
+          </tr>
+          <tr>
+            <div style={{ width: '40%', float: "right" }}>
+              <AddProject getData={() => this.getAllProjects()} /> {/* <== !!! */}
+            </div>
+          </tr>
+          <tr>
+            <div class='columns is-mobile is-centered'>
+              <div class='column is-6'>
+                <div class='list'>
+                  {this.state.listOfProjects.map(project => {
+                    return (
+                      <div key={project._id}>
+                        <Link to={`/projects/${project._id}`}>
+                          <td>
+                            <h3 class="list-item">{project.title}</h3>
+                          </td>
+                          <td>
+
+                          </td>
+                        </Link>
+                        {/* 🥁 added so the tasks can be displayed:  🥁 */}
+                        {/* <ul>
                   { project.tasks.map((task, index) => {
                     return <li key={index}>{task.title}</li>
                   }) }
                 </ul>  */}
-                          {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
-                        </div>
-                      )
-                    })
-                    }
-                  </div>
+                        {/* <p style={{maxWidth: '400px'}} >{project.description} </p> */}
+                      </div>
+                    )
+                  })
+                  }
                 </div>
               </div>
-            </tr>
-          </div>
+            </div>
+          </tr>
         </div>
+
       </table>
     )
   }
