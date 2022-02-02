f45bcf1aec852a61f0296bf33cd6da482cc76309
Tech Tupp <techtupp@gmail.com>
2022-02-02 23:39:09 +0100

    finish for todayy

--- client/src/components/projects/ProjectList.js
+++ client/src/components/projects/ProjectList.js
@@ -29,45 +29,45 @@ class ProjectList extends Component {
   render() {
     return (
       <table class="table">
-        <div>
 
-          <tr>
-            <div>
-              <h1>
-                Todo List
-              </h1>
-            </div>
-          </tr>
-          <tr>
-            <td>
-              {/* <div class='columns is-mobile is-centered'>
+
+        <tr>
+          <div>
+            <h1>
+              Todo List
+            </h1>
+          </div>
+        </tr>
+        <tr>
+          <td>
+            {/* <div class='columns is-mobile is-centered'>
                 <div class='column is-8'>
                   <div class='list'> */}
-                    {this.state.listOfProjects.map(project => {
-                      return (
+            {this.state.listOfProjects.map(project => {
+              return (
 
-                        <div key={project._id}>
-                        
-                            <Link to={`/projects/${project._id}`}>
-                              <h3 class="list-item">{project.title}</h3>
-                            </Link>
-                          
-                        </div>
+                <div key={project._id}>
 
-                      )
-                    })
-                    }
-               {/*    </div>
+                  <Link to={`/projects/${project._id}`}>
+                    <h3 class="list-item">{project.title}</h3>
+                  </Link>
+
+                </div>
+
+              )
+            })
+            }
+            {/*    </div>
                 </div>
               </div> */}
-            </td>
-            <td>
-              <div style={{ float: "right" }}>
-                <AddProject getData={() => this.getAllProjects()} /> {/* <== !!! */}
-              </div>
-            </td>
-          </tr>
-        </div>
+          </td>
+          <td>
+            <div style={{ float: "right" }}>
+              <AddProject getData={() => this.getAllProjects()} /> {/* <== !!! */}
+            </div>
+          </td>
+        </tr>
+
 
       </table>
     )
